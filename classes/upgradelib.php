0d6b437a3da0c3aacc58aebd868ef4ccbc226f70
TIm St.Clair <tim.stclair@gmail.com>
2022-11-09 13:12:40 +1100

    remove customfields

--- classes/upgradelib.php
+++ classes/upgradelib.php
@@ -31,56 +31,56 @@ class upgradelib {
      *
      * @throws \coding_exception
      */
-    public static function add_custom_user_fields() {
-        global $DB;
+    // public static function add_custom_user_fields() {
+    //     global $DB;
 
-        set_config('metadataenabled', true, 'metadatacontext_cohort');
+    //     set_config('metadataenabled', true, 'metadatacontext_cohort');
 
-        $categories = $DB->get_records('user_info_category', ['name' => get_string('pluginname', 'format_topicsactivitycards')],
-                'sortorder ASC');
+    //     $categories = $DB->get_records('user_info_category', ['name' => get_string('pluginname', 'format_topicsactivitycards')],
+    //             'sortorder ASC');
 
-        // Check that we have at least one category defined.
-        if (empty($categories)) {
-            $defaultcategory = new \stdClass();
-            $defaultcategory->name = get_string('pluginname', 'format_topicsactivitycards');
-            $defaultcategory->sortorder = (1 + $DB->get_field('local_metadata_category', 'max(sortorder)', []));
-            $defaultcategory->contextlevel = CONTEXT_MODULE;
-            $defaultcategory->id = $DB->insert_record('local_metadata_category', $defaultcategory);
-        } else {
-            $defaultcategory = reset($categories);
-        }
+    //     // Check that we have at least one category defined.
+    //     if (empty($categories)) {
+    //         $defaultcategory = new \stdClass();
+    //         $defaultcategory->name = get_string('pluginname', 'format_topicsactivitycards');
+    //         $defaultcategory->sortorder = (1 + $DB->get_field('local_metadata_category', 'max(sortorder)', []));
+    //         $defaultcategory->contextlevel = CONTEXT_MODULE;
+    //         $defaultcategory->id = $DB->insert_record('local_metadata_category', $defaultcategory);
+    //     } else {
+    //         $defaultcategory = reset($categories);
+    //     }
 
-        $fields = [
-                [
-                        'shortname'          => 'cardimage',
-                        'name'               => get_string('cardimage', 'format_topicsactivitycards'),
-                        'contextlevel'       => CONTEXT_MODULE,
-                        'datatype'           => 'file',
-                        'categoryid'         => $defaultcategory->id,
-                        'description'        => get_string('cardimagedescription', 'format_topicsactivitycards'),
-                        'descriptionformate' => FORMAT_MOODLE,
-                        'visible'            => 2,
-                        'param1'             => 1,
-                        'param2'             => 0
-                ],
-                [
-                        'shortname'          => 'duration',
-                        'name'               => get_string('duration', 'format_topicsactivitycards'),
-                        'contextlevel'       => CONTEXT_MODULE,
-                        'datatype'           => 'duration',
-                        'categoryid'         => $defaultcategory->id,
-                        'description'        => '',
-                        'descriptionformate' => FORMAT_MOODLE,
-                        'visible'            => 2,
-                        'param1'             => ''
-                ],
-        ];
+    //     $fields = [
+    //             [
+    //                     'shortname'          => 'cardimage',
+    //                     'name'               => get_string('cardimage', 'format_topicsactivitycards'),
+    //                     'contextlevel'       => CONTEXT_MODULE,
+    //                     'datatype'           => 'file',
+    //                     'categoryid'         => $defaultcategory->id,
+    //                     'description'        => get_string('cardimagedescription', 'format_topicsactivitycards'),
+    //                     'descriptionformate' => FORMAT_MOODLE,
+    //                     'visible'            => 2,
+    //                     'param1'             => 1,
+    //                     'param2'             => 0
+    //             ],
+    //             [
+    //                     'shortname'          => 'duration',
+    //                     'name'               => get_string('duration', 'format_topicsactivitycards'),
+    //                     'contextlevel'       => CONTEXT_MODULE,
+    //                     'datatype'           => 'duration',
+    //                     'categoryid'         => $defaultcategory->id,
+    //                     'description'        => '',
+    //                     'descriptionformate' => FORMAT_MOODLE,
+    //                     'visible'            => 2,
+    //                     'param1'             => ''
+    //             ],
+    //     ];
 
-        foreach ($fields as $field) {
-            if (!$DB->record_exists('local_metadata_field', array('shortname' => $field['shortname']))) {
-                $field['sortorder'] = (1 + $DB->get_field('local_metadata_field', 'max(sortorder)', ['categoryid' => $defaultcategory->id]));
-                $field['id'] = $DB->insert_record('local_metadata_field', (object) $field);
-            }
-        }
-    }
+    //     foreach ($fields as $field) {
+    //         if (!$DB->record_exists('local_metadata_field', array('shortname' => $field['shortname']))) {
+    //             $field['sortorder'] = (1 + $DB->get_field('local_metadata_field', 'max(sortorder)', ['categoryid' => $defaultcategory->id]));
+    //             $field['id'] = $DB->insert_record('local_metadata_field', (object) $field);
+    //         }
+    //     }
+    // }
 }
